969cd21cc30df4c26f4d34a8a4a989bdeeafc5c1
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2022-05-17 13:57:05 -0300

    Curriculo atualizado

--- 03_AUT_EST_ENTREGA/Semana 4/frontend/js/script.js
+++ 03_AUT_EST_ENTREGA/Semana 4/frontend/js/script.js
@@ -1,24 +1,18 @@
-let darkMode = localStorage.getItem('darkMode');
-const darkModeToggle = document.querySelector('#dark-mode-toggle');
-darkModeToggle.addEventListener('click', () => {
-    darkMode = localStorage.getItem('darkMode');
-    if (darkMode !== 'enabled' || darkMode == null) {
-        enableDarkMode();
-    } else {
-        disableDarkMode();
-    }
-});
-
-const enableDarkMode = () => {
-    document.body.classList.add('darkmode');
-    localStorage.setItem('darkMode', 'enabled');
+window.onload = function () {
+    var duration = 3; // Converter para segundos
+    document.getElementById("curriculo").style.display = 'none';
+    document.getElementById("loader").style.display = 'flex';
+    startTimer(duration); // iniciando o timer
 }
 
-const disableDarkMode = () => {
-    document.body.classList.remove('darkmode');
-    localStorage.setItem('darkMode', null);
-}
-
-if (darkMode === 'enabled') {
-    enableDarkMode();
+function startTimer(duration) {
+    var timer = duration, seconds;
+    var setIntervalo = setInterval(function () {
+        seconds = parseInt(timer % 60, 10);
+        if (--timer <= 0) {
+            clearInterval(setIntervalo);
+            document.getElementById("curriculo").style.display = 'block';
+            document.getElementById("loader").style.display = 'none';
+        }
+    }, 1000);
 }
--- 03_AUT_EST_ENTREGA/Semana 5/frontend/js/script.js
+++ 03_AUT_EST_ENTREGA/Semana 5/frontend/js/script.js
@@ -1,3 +1,22 @@
+window.onload = function () {
+    var duration = 3; // Converter para segundos
+    document.getElementById("curriculo").style.display = 'none';
+    document.getElementById("loader").style.display = 'flex';
+    startTimer(duration); // iniciando o timer
+}
+
+function startTimer(duration) {
+    var timer = duration, seconds;
+    var setIntervalo = setInterval(function () {
+        seconds = parseInt(timer % 60, 10);
+        if (--timer <= 0) {
+            clearInterval(setIntervalo);
+            document.getElementById("curriculo").style.display = 'block';
+            document.getElementById("loader").style.display = 'none';
+        }
+    }, 1000);
+}
+
 let darkMode = localStorage.getItem('darkMode');
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
 darkModeToggle.addEventListener('click', () => {
