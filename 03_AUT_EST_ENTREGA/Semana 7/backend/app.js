73aadb48bcda5e4d03eafedace29648d6aebf34a
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2022-06-01 12:06:41 -0300

    Curriculo com o metodo CRUD

--- 03_AUT_EST_ENTREGA/Semana 7/backend/app.js
+++ 03_AUT_EST_ENTREGA/Semana 7/backend/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 /****** CRUD ******************************************************************/
 
-// Retorna todos registros 
+// Retorna todos infos 
 app.get('/getDados', (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
@@ -35,7 +35,71 @@ app.get('/getDados', (req, res) => {
 	db.close(); // Fecha o banco
 });
 
+app.post('/insertLinguagem', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
 
+	sql = "INSERT INTO TB_LINGUAGENS (name) VALUES ('" + req.body.name + "')";
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [], err => {
+		if (err) {
+			throw err;
+		}
+	});
+	db.close(); // Fecha o banco
+	res.end();
+});
+
+// Retorna todos linguagens
+app.get('/getLinguagens', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); //
+
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	var sql = 'SELECT * FROM TB_LINGUAGENS';
+	db.all(sql, [], (err, rows) => {
+		if (err) {
+			throw err;
+		}
+
+		res.json(rows);
+
+	});
+	db.close(); // Fecha o banco
+});
+
+// app.get("/bem", function(req,res){
+
+// })
+app.post('/deleteLinguagem', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "DELETE FROM TB_LINGUAGENS WHERE id = " + req.body.id;
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [], err => {
+		if (err) {
+			throw err;
+		}
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
+
+app.post('/updateLinguagem', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	//res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "UPDATE TB_LINGUAGENS SET name = '" + req.body.name + "' WHERE id = " + req.body.id;
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [], err => {
+		if (err) {
+			throw err;
+		}
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
 
 
 
--- 03_AUT_EST_ENTREGA/Semana 7/frontend/js/script.js
+++ 03_AUT_EST_ENTREGA/Semana 7/frontend/js/script.js
@@ -1,10 +1,12 @@
+var api = 'http://127.0.0.1:3061';
+
 window.onload = function () {
-    $.get("/getDados", function(resultado){
+    $.get("/getDados", function (resultado) {
         $('#name').html(resultado[0].name);
         $('#email').html(resultado[0].email);
         $('#phone').html(resultado[0].phone);
     });
-    
+    metodos.get();
     var duration = 3; // Converter para segundos
     document.getElementById("curriculo").style.display = 'none';
     document.getElementById("loader").style.display = 'flex';
@@ -23,6 +25,7 @@ function startTimer(duration) {
     }, 1000);
 }
 
+/*Modo noturno*/
 let darkMode = localStorage.getItem('darkMode');
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
 darkModeToggle.addEventListener('click', () => {
@@ -46,4 +49,91 @@ const disableDarkMode = () => {
 
 if (darkMode === 'enabled') {
     enableDarkMode();
+}
+
+var metodos = {
+    insert(name) {
+        $.ajax({
+            type: 'POST',
+            url: api + '/insertLinguagem',
+            data: { name: name },
+        }).done(function () {
+            metodos.get();
+        }).fail(function (msg) {
+            //console.log('FAIL');
+        }).always(function (msg) {
+            //console.log('ALWAYS');
+        });
+
+    },
+    get() {
+        $.ajax({
+            url: api + '/getLinguagens',
+            type: 'GET',
+            success: data => {
+                var list = '';
+                data.forEach(element => {
+                    list += `<li class="d-flex">${element.name}<div class="w-100"><button class="pull-right" id="btn-excluir" onclick="metodos.delete(${element.id})"><i class="fa fa-times" aria-hidden="true"></i></button><button class="pull-right" id="btn-edit" onclick="adicionarLinguagem(${element.id}, '${element.name}')"><i class="fa fa-pencil" aria-hidden="true"></i></button></div></li>`;
+                });
+                $('#linguagens').html(list);
+            }
+        });
+
+    },
+    delete(id) {
+
+        if (confirm('Confirma a exclusão?')) {
+            $.ajax({
+                type: 'POST',
+                url: api + '/deleteLinguagem',
+                data: { id: id },
+            }).done(function () {
+                metodos.get();
+            }).fail(function (msg) {
+                //console.log('FAIL');
+            }).always(function (msg) {
+                //console.log('ALWAYS');
+            });
+        }
+    },
+    update(id, name) {
+
+
+        $.ajax({
+            type: 'POST',
+            url: api + '/updateLinguagem',
+            data: { name: name, id: id },
+        }).done(function () {
+            metodos.get();
+        }).fail(function (msg) {
+            //console.log('FAIL');
+        }).always(function (msg) {
+            //console.log('ALWAYS');
+        });
+
+
+    },
+}
+
+function adicionarLinguagem(id, name) {
+    if (name == null) {
+        name = '';
+    }
+    var input = '<div class="input"><div class="d-flex"><input type="text" id="nameLinguagem" value="' + name + '" placeholder="adicionar uma nova"><button class="pull-right" id="btn-excluir" onclick="removerLinguagem(' + id + ')"><i class="fa fa-times"aria-hidden="true"></i></button><button onclick="saveLinguagem(' + id + ')" class="pull-right" id="btn-edit"><i class="fa fa-floppy-o"aria-hidden="true"></i></button></div></div>';
+    document.getElementById("input").innerHTML = input;
+}
+
+function removerLinguagem() {
+    var input = '';
+    $("#input").html(input);
+}
+
+function saveLinguagem(id) {
+    var name = document.getElementById("nameLinguagem").value;
+    if (id != null) {
+        metodos.update(id, name);
+    } else {
+        metodos.insert(name);
+    }
+    removerLinguagem()
 }
