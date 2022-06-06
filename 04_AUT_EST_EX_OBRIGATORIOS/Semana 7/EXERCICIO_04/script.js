800ad29818b89554220b0df7316420c0078e450b
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2022-06-06 13:55:18 -0300

    Exercicio 4

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/EXERCICIO_04/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("#btn").on("click", function () {
+        var total = 0;
+        var quant = parseInt($("#quantidade").val());
+        var periodo = $("#periodo").val();
+        if (periodo == "Diurno" && quant < 50) {
+            total = ((quant * 200) * 0.6);
+        } else if (periodo == "Noturno" && quant < 50) {
+            total = ((quant * 100) * 0.8);
+        }
+        $("#resultado").html(total);
+    });
+});
